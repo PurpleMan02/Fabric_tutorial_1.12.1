f05079584a822b7e3810e464265be55adaa503fd
PurpleMan02 <kfirtil@gmail.com>
2025-11-18 18:22:24 +0200

    try again

---
+++ src/main/java/net/purple/tutorial/tutorialModClient.java
@@ -0,0 +1,10 @@
+package net.purple.tutorial;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class tutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
